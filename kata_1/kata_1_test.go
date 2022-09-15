9cf69403eabbed29587cbf71d9bf37af94622dc7
shaw-malcolm <shaw.malcolm@accenture.com>
2022-09-15 16:27:00 +0100

    Create bug to test linter

--- kata_1/kata_1_test.go
+++ kata_1/kata_1_test.go
@@ -1,13 +1,16 @@
 package kata1
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestKata1(t *testing.T) {
 	t.Run("description", func(t *testing.T) {
+		actual := kata1(2, 2)
+		expected := []int{2}
 
-	})
-
-	t.Run("description", func(t *testing.T) {
+		assert.Equal(t, expected, actual, "There should be one item in the slice")
 
 	})
 }
